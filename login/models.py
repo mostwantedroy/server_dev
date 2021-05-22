e5dfbf72e1a3ca80637d3563bac4430f35fc47c6
Roy <roy7579@gmail.com>
2021-05-22 19:47:24 +0900

    add app_login

--- login/models.py
+++ login/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class LoginUser(models.Model):
     user_id = models.CharField(max_length = 20, null = False, default = False)
-    user_pw = models.CharField(max_length = 20, null = False, default = False)
+    user_pw = models.CharField(max_length = 256, null = False, default = False)
 
     class Meta:
         db_table = 'login_user'
--- login/views.py
+++ login/views.py
@@ -7,10 +7,27 @@ from rest_framework.response import Response
 from .models import LoginUser
 from django.contrib.auth.hashers import make_password, check_password
 
+class AppLogin(APIView):
+    def post(self, request):
+        user_id = request.data.get('user_id')
+        user_pw = request.data.get('user_pw')
+
+        user = LoginUser.objects.filter(user_id = user_id).first()
+
+        if not user:
+            return Response(dict(msg="해당 사용자가 없습니다."))
+
+        if check_password(user_pw, user.user_pw):
+            return Response(dict(msg="로그인 성공"))
+        else:
+            return Response(dict(msg="비밀번호가 틀렸습니다."))
+
+
 class RegistUser(APIView):
     def post(self, request):
         user_id = request.data.get('user_id')
         user_pw = request.data.get('user_pw')
+        user_pw_encrypted = make_password(user_pw)
 
         # user_id에 대한 검증 로직이 들어감.
         #
@@ -20,11 +37,11 @@ class RegistUser(APIView):
         if user is not None:
             return Response(dict(msg="동일한 아이디가 있습니다."))
 
-        LoginUser.objects.create(user_id=user_id, user_pw=user_pw)
+        LoginUser.objects.create(user_id=user_id, user_pw=user_pw_encrypted)
 
         data = dict(
             user_id=user_id,
-            user_pw=user_pw
+            user_pw=user_pw_encrypted
         )
 
         return Response(data)
