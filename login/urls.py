ce7c43940b954bde2107b57d74833a6316a4f0bf
Roy <roy7579@gmail.com>
2021-05-23 18:40:10 +0900

    created todo views.py

--- login/urls.py
+++ login/urls.py
@@ -3,5 +3,5 @@ from .views import RegistUser, AppLogin
 
 urlpatterns = [
     url('regist_user', RegistUser.as_view(), name = 'regist_user'),
-    url('app_login', AppLogin.as_view(), name = 'app_login')
+    url('app_login', AppLogin.as_view(), name = 'app_login'),
 ]
